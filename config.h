d37a401ef6e3670195ed2320a1da008ef03843a8
v02v02 <VictorVar@proton.me>
2025-08-14 20:13:45 -0400

    Initial commit

---
+++ config.h
@@ -0,0 +1,35 @@
+/* See LICENSE file for copyright and license details. */
+
+/* v02v02's build of dmenu.
+ *
+ * Tweaks made:
+ * - Set the 2nd value of "[SchemeSel]" to "#ff7500".
+ *   (The color orange is now the background color of highlighted options.)
+ *
+ * Patches applied:
+ * - None currently
+ *
+ */
+
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#ff7500" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
